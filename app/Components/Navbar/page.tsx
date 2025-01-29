9340909f4ed279e081f8c072a927adcca3a554e6
BRYOOH <bryoohmuchira@gmail.com>
2025-01-29 22:05:58 +0300

    Navbar:Useclient

--- app/Components/Navbar/page.tsx
+++ app/Components/Navbar/page.tsx
@@ -1,7 +1,6 @@
+"use client";
 import Image from "next/image"
 import tracker from '@/app/Assets/tracker.jpeg'
-
-import { useNavigate } from 'react-router-dom'
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 
